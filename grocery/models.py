fdf22fb8ddb8a55888419d0a5a1116b74bcf49b3
sumoondev <sumoon.dev@gmail.com>
2026-03-06 22:40:26 +0545

    fix: Rename

--- grocery/models.py
+++ grocery/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class GroceryItem(models.Model):
     name = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
--- groceryApi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GroceryapiConfig(AppConfig):
-    name = 'groceryApi'
