186d15a0e0946922144d2b3e36e9f2f887619d6a
sumoondev <sumoon.dev@gmail.com>
2026-03-07 22:34:03 +0545

    fix: rename requirements.txt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 import { nanoid } from "nanoid";
 import Form from "./components/Form";
 
-const BASE_URL = "http://127.0.0.1:8000/api/grocery";
+const BASE_URL = "https://crud-restapi-542j.onrender.com";
 
 const App = () => {
     const [items, setItems] = useState([]);
